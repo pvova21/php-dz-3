dde141ad192509d6277dd854d6b4e647bce25fe5
alex <vpodrezov@jeepshop.local>
2023-10-24 12:19:28 +0300

    fix

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-$фамилия = "подрезов";
-$имя = "владимир";
-$отчество = "юрьевич";
-
 // Установим кодировку UTF-8
-//mb_internal_encoding("UTF-8");
+mb_internal_encoding("UTF-8");
+
+// Ввод данных с консоли
+$surname = readline("Введите фамилию: ");
+$name = readline("Введите имя: ");
+$patronymic = readline("Введите отчество: ");
 
 // Преобразуем первые буквы
-$первая_буква_фамилии = mb_strtoupper(mb_substr($фамилия, 0, 1));
-$первая_буква_имени = mb_strtoupper(mb_substr($имя, 0, 1));
-$первая_буква_отчества = !empty($отчество) ? mb_strtoupper(mb_substr($отчество, 0, 1)) : "";
+$первая_буква_фамилии = mb_strtoupper(mb_substr($surname, 0, 1));
+$первая_буква_имени = mb_strtoupper(mb_substr($name, 0, 1));
+$первая_буква_отчества = !empty($patronymic) ? mb_strtoupper(mb_substr($patronymic, 0, 1)) : "";
 
 // Формируем надпись
-$fullName = mb_ucfirst($фамилия) . ' ' . mb_ucfirst($имя) . ' ' . mb_ucfirst($отчество);
-$surnameAndInitials = "Фамилия и инициалы: " . mb_ucfirst($фамилия) . " $первая_буква_имени.$первая_буква_отчества.";
+$fullName = mb_ucfirst($surname) . ' ' . mb_ucfirst($name) . ' ' . mb_ucfirst($patronymic);
+$surnameAndInitials = "Фамилия и инициалы: " . mb_ucfirst($surname) . " $первая_буква_имени.$первая_буква_отчества.";
 $fio = $первая_буква_фамилии . $первая_буква_имени . $первая_буква_отчества;
 
 echo "Полное имя: '$fullName'\n";
@@ -24,5 +25,4 @@ echo "Аббревиатура: '$fio'";
 function mb_ucfirst($str) {
     return mb_strtoupper(mb_substr($str, 0, 1)) . mb_substr($str, 1);
 }
-
 ?>
